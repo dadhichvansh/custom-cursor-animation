b636b2e0bbe48d61f68a09e04dd15b83eaac3248
vansh-home <dadhichvansh46@gmail.com>
2025-03-23 17:50:10 +0530

    Add custom cursor movement functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+const container = document.querySelector("#container");
+const cursor = document.querySelector("#cursor");
+
+container.addEventListener("mousemove", (e) => {
+  gsap.to(cursor, {
+    x: e.x,
+    y: e.y,
+    duration: 1,
+    ease: "back.out(1)",
+  });
+});
