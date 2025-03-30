472c5c7febd37e3508f2e56ee642679a95fdd7bf
vansh-home <dadhichvansh46@gmail.com>
2025-03-30 21:48:05 +0530

    Added advanced cursor animation

--- src/main.js
+++ src/main.js
@@ -2,12 +2,30 @@ import "./style.css";
 
 const container = document.querySelector("#container");
 const cursor = document.querySelector("#cursor");
+const imageDiv = document.querySelector("#image");
 
 container.addEventListener("mousemove", (e) => {
   gsap.to(cursor, {
     x: e.x,
     y: e.y,
-    duration: 1,
-    ease: "back.out(1)",
+    duration: 0.6,
+  });
+});
+
+imageDiv.addEventListener("mouseenter", () => {
+  cursor.innerHTML = "View More";
+  gsap.to(cursor, {
+    scale: 2,
+    duration: 0.6,
+    backgroundColor: "#ffe3c280",
+  });
+});
+
+imageDiv.addEventListener("mouseleave", () => {
+  cursor.innerHTML = "";
+  gsap.to(cursor, {
+    scale: 1,
+    duration: 0.6,
+    backgroundColor: "#ffe4c4",
   });
 });
